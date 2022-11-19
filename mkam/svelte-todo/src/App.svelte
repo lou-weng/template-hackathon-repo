1c2c1dc25b3d0635258f6894e64e46051250e35a
m20arcusk <marcuskam20@gmail.com>
2022-11-19 12:47:14 -0800

    svelte to do list

---
+++ mkam/svelte-todo/src/App.svelte
@@ -0,0 +1,60 @@
+
+<script>
+	import Header from "./components/Header.svelte";
+	import List from "./components/List.svelte";
+	</script>
+
+<style>
+	.container{
+		width: 500px;
+		background: #009579;
+	}
+
+	:global(.container *) {
+		font-family: sans-serif;
+	}
+</style>
+
+<div class="container">
+	<Header />
+	<List />
+</div>
+
+<!--
+<script>
+	let name = 'Marcus';
+</script>
+
+<main>
+	<h1>{name}'s To Do List: </h1>
+	<p>1. Wake up</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	p {
+		color: black;
+		font-size: 2em;
+		font-weight: 100;
+		text-align: center;
+	}
+	h1 {
+		color: blue;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 250;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+-->
